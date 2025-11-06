080db7d07e95a986b136f7cf6efc760be37ac2fa
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-06 08:13:05 +0530

    Create main.tf

---
+++ 00-SWS/Installations/AL-23/Terraform/Maven/main.tf
@@ -0,0 +1,68 @@
+module "maven_sg" {
+  source         = "git::https://github.com/vaheedgithubac/Infra.git//modules/security_group"
+  vpc_id         = var.vpc_id  #local.vpc_id
+  sg_name        = "maven_sg"
+  sg_description = "Maven Security Group"
+
+  project_name = var.project_name
+  env          = var.env
+  common_tags  = var.common_tags
+}
+
+# Maven should be accessed from office n/w   0.0.0.0/0 ---> jenkins
+resource "aws_security_group_rule" "maven_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.maven_sg.sg_id
+}
+
+module "maven_ec2" {
+  source = "git::https://github.com/vaheedgithubac/Infra.git//modules/ec2"
+
+  ami_id                      = var.ami_id
+  instance_type               = var.instance_type
+  public_key_name             = var.public_key_name
+  sg_id                       = [module.maven_sg.sg_id] #[local.sg_id]
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
+  what_type_instance          = "maven"
+
+  # user_data = file("${path.module}/ansible_setup.sh")
+
+  # is_nat_instance             = var.is_nat_instance  # creates NAT instance if true
+
+
+  project_name = var.project_name
+  env          = var.env
+  common_tags  = var.common_tags
+}
+
+
+resource "null_resource" "maven_provisioner" {
+  depends_on = [module.maven_ec2]
+
+  # Changes to any instance of the instance requires re-provisioning
+  triggers = {
+    instance_id = module.maven_ec2.ec2_instance.id
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = module.maven_ec2.ec2_instance.private_ip
+    private_key = file(var.private_key_path)
+    timeout     = "5m"
+  }
+
+  provisioner "local-exec" {
+    # echo "\${module.jenkins_ec2.ec2_instance.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=\${var.private_key_path}" >> inventory.ini
+
+    command = <<-EOT
+      echo " Maven installation started... "
+      ansible-playbook -i ${module.maven_ec2.ec2_instance.private_ip}, maven_setup.yml
+    EOT
+  }
+}
