f49c4e915c34b456b5cbaca585ec8c0ade4af7d7
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-20 21:54:49 +0530

    Update app_health_check.sh

--- 01-expense/Dockerfiles/files/health_checks/app_health_check.sh
+++ 01-expense/Dockerfiles/files/health_checks/app_health_check.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-curl -f localhost:5000/health
+
+# Exit non-zero if health endpoint fails
+curl -fsSL localhost:8080/health
+
+# -f → fail on HTTP errors
+# -s → silent
+# -S → show error if it fails
+# -L → Follow redirects
